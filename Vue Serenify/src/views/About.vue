7892d549adfc69493e1055322098a9ed16a0f6ee
Faruq Azizan Riyansa <faruq7j@gmail.com>
2024-02-27 21:21:50 +0700

    update

--- Vue Serenify/src/views/About.vue
+++ Vue Serenify/src/views/About.vue
@@ -1,1 +1,51 @@
-<template>INI ABOUT PAGE</template>
+<template>
+  <div class="container-fluid">
+    <div class="row margin">
+      <div class="col-sm-3"></div>
+      <div class="col-sm-6 margin">
+        <h1 style="text-align: center">Kami Merancang Cara Baru Dalam Berkonsultasi</h1>
+      </div>
+      <div class="col-sm-3"></div>
+    </div>
+    <div class="row margin">
+      <div class="col-sm-3"></div>
+      <div class="col-sm-6 margin-text">
+        <p style="text-align: center" class="justify-content-center">
+          Lorem ipsum dolor sit amet consectetur. Ipsum blandit nisl varius aliquet hac ut tortor
+          sodales. Tellus tristique nisl urna vitae tincidunt pulvinar tellus id. Suspendisse a
+          vehicula sagittis nunc etiam cum.
+        </p>
+      </div>
+    </div>
+    <div
+      class="row bg-image p-5 shadow-1-strong rounded mb-5 text-white margin"
+      style="background-image: url('https://mdbcdn.b-cdn.net/img/new/slides/003.webp')"
+    >
+      <div class="col-sm-6">
+        <h1 class="mb-3 h2">Temukan yang kamu butuhkan</h1>
+
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus praesentium labore
+          accusamus sequi, voluptate debitis tenetur in deleniti possimus modi voluptatum neque
+          maiores dolorem unde? Aut dolorum quod excepturi fugit.
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-1"></div>
+      <div class="col-sm-5 margin"><h2 style="text-align: center">Cerita Kami</h2></div>
+      <div class="col-sm-4 margin">
+        <p>
+          Lorem ipsum dolor sit amet consectetur. In sem proin consectetur arcu sagittis ultricies
+          velit. Nec sodales eget quisque ultrices tristique est interdum fames a. Ipsum nunc felis
+          magna faucibus hendrerit at molestie mollis nunc. Vitae lectus egestas leo at sed ipsum
+          ut. Et morbi sem auctor integer in facilisi pellentesque pellentesque dui. Volutpat turpis
+          porttitor viverra etiam nunc gravida blandit. Dictumst viverra fringilla velit lacinia
+          eget lorem ornare commodo blandit. Duis viverra tellus ut quis elementum sagittis diam
+          nulla mattis. Quam facilisis dictum orci molestie libero accumsan diam vulputate.
+        </p>
+      </div>
+      <div class="col-sm-2"></div>
+    </div>
+  </div>
+</template>
--- Vue Serenify/src/views/HomeView.vue
+++ Vue Serenify/src/views/HomeView.vue
@@ -3,5 +3,72 @@ import HomeContent from '@/components/HomeContent.vue'
 </script>
 
 <template>
-  <HomeContent></HomeContent>
+  <div class="container-fluid">
+    <div class="row margin">
+      <div class="col-sm-3"></div>
+      <div class="col-sm-6 margin">
+        <h1 style="text-align: center">Setiap Langkamu Adalah Tanda Keberanian.</h1>
+      </div>
+      <div class="col-sm-3"></div>
+    </div>
+    <div class="row">
+      <div class="col-sm-3"></div>
+      <div class="col-sm-6 margin-text">
+        <p style="text-align: center" class="justify-content-center">
+          Lorem ipsum dolor sit amet consectetur. Ipsum blandit nisl varius aliquet hac ut tortor
+          sodales. Tellus tristique nisl urna vitae tincidunt pulvinar tellus id. Suspendisse a
+          vehicula sagittis nunc etiam cum.
+        </p>
+      </div>
+      <div class="col-sm-3"></div>
+    </div>
+    <div class="row margin">
+      <div class="col">
+        <ul class="nav justify-content-center margin">
+          <li class="nav-1">
+            <a class="nav-link" href="#">
+              <button type="button" class="btn btn-outline-primary">Daftar</button>
+            </a>
+          </li>
+          <li class="nav-1">
+            <a class="nav-link" href="#"
+              ><button type="button" class="btn btn-primary">Langganan</button></a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div
+      class="row bg-image p-5 shadow-1-strong rounded mb-5 text-white margin"
+      style="background-image: url('https://mdbcdn.b-cdn.net/img/new/slides/003.webp')"
+    >
+      <div class="col-sm-6">
+        <h1 class="mb-3 h2">Temukan yang kamu butuhkan</h1>
+
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus praesentium labore
+          accusamus sequi, voluptate debitis tenetur in deleniti possimus modi voluptatum neque
+          maiores dolorem unde? Aut dolorum quod excepturi fugit.
+        </p>
+      </div>
+    </div>
+    <div class="row margin">
+      <div class="col-sm-3"></div>
+      <div class="col-sm-6 margin">
+        <h1 style="text-align: center">Setiap Langkamu Adalah Tanda Keberanian.</h1>
+      </div>
+      <div class="col-sm-3"></div>
+    </div>
+    <div class="row">
+      <div class="col-sm-3"></div>
+      <div class="col-sm-6 margin-text">
+        <p style="text-align: center" class="justify-content-center">
+          Lorem ipsum dolor sit amet consectetur. Ipsum blandit nisl varius aliquet hac ut tortor
+          sodales. Tellus tristique nisl urna vitae tincidunt pulvinar tellus id. Suspendisse a
+          vehicula sagittis nunc etiam cum.
+        </p>
+      </div>
+      <div class="col-sm-3"></div>
+    </div>
+  </div>
 </template>
